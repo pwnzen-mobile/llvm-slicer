c160b2dbbc1868250ffaae0d1c0bb7f2a85ce273
tangzhushou <ellison.tang@gmail.com>
2019-09-03 00:18:37 +0800

    Update llvm-slicer.cpp
    I hand on this project and will commit every once in a while.

--- tools/llvm-slicer/llvm-slicer.cpp
+++ tools/llvm-slicer/llvm-slicer.cpp
@@ -43,6 +43,52 @@
 
 using namespace llvm;
 
+// https://llvm.org/docs/CommandLine.html#positional-options
+static cl::opt<bool>
+EnableIntra("enable-intra",
+            cl::desc("Enable intra-procedure analysis"));
+
+// errs() << EnableIntra << '\n';
+// value is initialized with the defaut constructor, that is 0
+// use argument -enable-intra, -enable-intra=true or -enable-intra=false
+
+/*
+./llvm-slicer -help
+OVERVIEW: llvm .ll -> .bc assembler
+
+USAGE: llvm-slicer [options] <input .llvm file>
+
+OPTIONS:
+
+General options:
+
+-enable-hcd            - Enable the hybrid cycle detection algorithm
+-enable-hu             - Enable the HU constraint optimization
+-enable-hvn            - Enable the HVN constraint optimization
+-enable-intra          - Enable intra-procedure analysis
+-enable-lcd            - Enable the lazy cycle detection algorithm
+-enable-objc-arc-opts  - enable/disable all ARC Optimizations
+-enable-scoped-noalias -
+-enable-tbaa           -
+-o=<filename>          - Override output filename
+-print-after-all       - Print IR after each pass
+-print-before-all      - Print IR before each pass
+-r=<report>            - Path to HTML report output file
+-rng-seed=<seed>       - Seed for the random number generator
+-stats                 - Enable statistics output from program (available with Asserts)
+-time-passes           - Time each pass, printing elapsed time for each on exit
+-verify-dom-info       - Verify dominator info (time consuming)
+-verify-loop-info      - Verify loop info (time consuming)
+-verify-scev           - Verify ScalarEvolution's backedge taken counts (slow)
+
+Generic Options:
+
+-help                  - Display available options (-help-hidden for more)
+-help-list             - Display list of available options (-help-list-hidden for more)
+-version               - Display the version of this program
+
+*/
+
 static cl::opt<std::string>
 InputFilename(cl::Positional, cl::desc("<input .llvm file>"), cl::init("-"));
 
@@ -51,20 +97,25 @@ static cl::opt<std::string>
                        cl::value_desc("filename"));
 
 int main(int argc, char **argv) {
-  // Print a stack trace if we signal out.
-  sys::PrintStackTraceOnErrorSignal();
-  PrettyStackTraceProgram X(argc, argv);
-  llvm_shutdown_obj Y;  // Call llvm_shutdown() on exit.
-  cl::ParseCommandLineOptions(argc, argv, "llvm .ll -> .bc assembler\n");
+    // Print a stack trace if we signal out.
+    sys::PrintStackTraceOnErrorSignal();
+    PrettyStackTraceProgram X(argc, argv);
+
+    // https://llvm.org/docs/ProgrammersManual.html#ending-execution-with-llvm-shutdown
+    llvm_shutdown_obj Y;  // Call llvm_shutdown() on exit.
+    // https://llvm.org/docs/CommandLine.html#the-cl-parsecommandlineoptions-function
+    cl::ParseCommandLineOptions(argc, argv, "llvm .ll -> .bc assembler\n");
     SMDiagnostic Err;
+    // https://llvm.org/docs/BitCodeFormat.html
     std::unique_ptr<Module> Mod = parseIRFile(InputFilename, Err, getGlobalContext());
     if (!Mod) {
         Err.print(argv[0], errs());
         return 1;
     }
-
+    // https://llvm.org/docs/WritingAnLLVMPass.html#what-passmanager-does
     legacy::PassManager *PM = new legacy::PassManager();
-
+  
+    // https://llvm.org/docs/Passes.html
     PM->add(new PostDominatorTree());
     PM->add(new PostDominanceFrontier());
     PM->add(new LoopInfoWrapperPass());
